e1f7f7f81615f94aa381669c76c0eeed3c5e8063
Shiryaev Aleksey <aleksi.aleksi2014@yandex.ru>
2026-02-14 17:33:05 +0300

    addition trouble plugin config

---
+++ lua/plugins/trouble.lua
@@ -0,0 +1,59 @@
+return {
+  "folke/trouble.nvim",
+  opts = {
+    auto_close = false, -- auto close when there are no items
+    auto_open = false, -- auto open when there are items
+    auto_preview = true, -- automatically open preview when on an item
+    auto_refresh = true, -- auto refresh when open
+    auto_jump = false, -- auto jump to the item when there's only one
+    focus = false, -- Focus the window when opened
+    restore = true, -- restores the last location in the list when opening
+    follow = true, -- Follow the current item
+    indent_guides = true, -- show indent guides
+    max_items = 200, -- limit number of items that can be displayed per section
+    multiline = true, -- render multi-line messages
+    pinned = false, -- When pinned, the opened trouble window will be bound to the current buffer
+    warn_no_results = true, -- show a warning when there are no results
+    open_no_results = false,
+  }, -- for default options, refer to the configuration section for custom setup.
+  cmd = "Trouble",
+  keys = {
+    {
+      "<leader>xx",
+      "<cmd>Trouble diagnostics toggle<cr>",
+      desc = "Diagnostics (Trouble)",
+    },
+    {
+      "<leader>xX",
+      "<cmd>Trouble diagnostics toggle filter.buf=0<cr>",
+      desc = "Buffer Diagnostics (Trouble)",
+    },
+    {
+      "<leader>cs",
+      "<cmd>Trouble symbols toggle focus=false<cr>",
+      desc = "Symbols (Trouble)",
+    },
+    {
+      "<leader>cl",
+      "<cmd>Trouble lsp toggle focus=false win.position=right<cr>",
+      desc = "LSP Definitions / references / ... (Trouble)",
+    },
+    {
+      "<leader>xL",
+      "<cmd>Trouble loclist toggle<cr>",
+      desc = "Location List (Trouble)",
+    },
+    {
+      "<leader>xQ",
+      "<cmd>Trouble qflist toggle<cr>",
+      desc = "Quickfix List (Trouble)",
+    },
+  },
+
+  {
+    "rachartier/tiny-inline-diagnostic.nvim",
+    event = "VeryLazy",
+    priority = 1000,
+    opts = {},
+  },
+}
