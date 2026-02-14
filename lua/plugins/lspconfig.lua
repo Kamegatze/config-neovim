150b8d23b9caa09339346afcd5173c9a525c2ecb
Shiryaev Aleksey <aleksi.aleksi2014@yandex.ru>
2026-02-14 17:27:51 +0300

    correction run lsp for rust

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -1,11 +1,12 @@
 return {
   "neovim/nvim-lspconfig",
   opts = {
+    diagnostics = { virtual_text = false },
     servers = {
       bacon_ls = {
-        enabled = diagnostics == "bacon-ls",
+        enabled = vim.g.lazyvim_rust_diagnostics == "bacon-ls",
       },
-      rust_analyzer = { enabled = false },
+      rust_analyzer = { enabled = vim.g.lazyvim_rust_diagnostics == "rust-analyzer" },
     },
   },
 }
--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -4,7 +4,7 @@ return {
   opts = function(_, opts)
     opts.ensure_installed = opts.ensure_installed or {}
     vim.list_extend(opts.ensure_installed, { "codelldb" })
-    if diagnostics == "bacon-ls" then
+    if vim.g.lazyvim_rust_diagnostics == "bacon-ls" then
       vim.list_extend(opts.ensure_installed, { "bacon" })
     end
   end,
